53ae7a1f7d39756a8ac116db51c5550eea55b392
mekbalidris <mekbalidriss@gmail.com>
2025-02-15 22:58:43 +0100

    .

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,19 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorizatino')
+    const token = req.header('Authorization');
 
-    if(!token) {
-        return res.status(401).json({ errors: {auth: "No token, authorization denied"}})
+    if (!token) {
+        return res.status(401).json({ errors: { auth: "No token, authorization denied" } });
     }
 
-    try{
-        const decoded = jwt.verify(token, "secret-key")
-        req.user = decoded.id 
-        next()
-    }catch(err){
-        res.status(401).json({ errors: { auth: "Token is not valid"}})
+    try {
+        const decoded = jwt.verify(token, "secret-key");
+        req.user = decoded.id;
+        next();
+    } catch (err) {
+        res.status(401).json({ errors: { auth: "Token is not valid" } });
     }
-}
+};
 
-module.exports = authMiddleware
+module.exports = authMiddleware;
