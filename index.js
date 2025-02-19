a98979e8fded79e617969a2317e5f36f0ec83b93
mekbalidris <mekbalidriss@gmail.com>
2025-02-19 17:58:58 +0100

    last changes

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require('dotenv').config()
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://restaurantly-git-main-idris-projects-88099c98.vercel.app", 
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+}));
 
 connectDB();
 
