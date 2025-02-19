0c37fc8b22dfdea69dd08cbf4a40f03f818ca349
mekbalidris <mekbalidriss@gmail.com>
2025-02-19 17:45:10 +0100

    fix bcrypt issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/users.js')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const generateToken = require('../utils/generateToken')
 
